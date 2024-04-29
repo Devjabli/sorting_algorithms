737fd085327e39f9a80b439aca83f7a2b3a601a3
otmane <aroussiotmane27@gmail.com>
2024-04-29 13:04:18 +0100

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting an array integers by ascending order
+ * @array: array to sorted
+ * @size: size of array
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j;
+
+	while (i < size - 1)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int tmp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,3 +14,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
