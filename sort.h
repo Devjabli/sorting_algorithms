5302d78d3a1f356b23a6b19196dfbde0ca9ceae1
otmane <aroussiotmane27@gmail.com>
2024-04-29 13:09:08 +0100

    bubble_sort

--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -7,14 +10,16 @@
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
- */
+*/
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+#endif
