883728da620d0482a8af80b33e4d70453f8b08ba
otmane <aroussiotmane27@gmail.com>
2024-04-29 13:42:45 +0100

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,7 @@ void bubble_sort(int *array, size_t size)
 
 	while (i < size - 1)
 	{
+		true;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -21,6 +22,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				print_array(array, size);
+				false;
 			}
 		}
 		i++;
--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 /**
  * struct listint_s - Doubly linked list node
